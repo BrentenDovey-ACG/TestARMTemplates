3979e298f377a89e8b69edf4bf0c73a385447107
BrentenDovey-ACG <68392307+BrentenDovey-ACG@users.noreply.github.com>
2022-06-28 16:51:38 -0500

    Update install-ADDS.ps1

--- install-ADDS.ps1
+++ install-ADDS.ps1
@@ -2,6 +2,7 @@
 # Windows PowerShell script for AD DS Deployment
 #
 
+Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
@@ -15,19 +16,3 @@ Install-ADDSForest `
 -NoRebootOnCompletion:$false `
 -SysvolPath "C:\Windows\SYSVOL" `
 -Force:$true
-
-#
-# Turn of IE ESC on Local Server for Internet Explorer
-#
-
-$AdminKey = "HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
-
-$UserKey = "HKLM:SOFTWAREMicrosoftActive SetupInstalled Components{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
-
-Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
-
-Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-
-Stop-Process -Name Explorer
-
-Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
