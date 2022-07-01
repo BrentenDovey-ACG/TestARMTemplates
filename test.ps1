99958ef6ab83a97a603188f23e71a6e02d1f69dd
BrentenDovey-ACG <68392307+BrentenDovey-ACG@users.noreply.github.com>
2022-07-01 09:43:46 -0500

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,4 @@
+New-Item C:\temp\test\test.txt
+New-Item C:\temp\test\sanityCheck.txt
+Set-Content C:\temp\test\test.txt "$(adminCreds.adminPassword)"
+Set-Content C:\temp\test\sanityCheck.txt 'If you see this I promise you are sane!'
